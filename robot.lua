13a22019894f7769c83810f44de3ae6ce46d60f2
NightStrix <nightstrx@gmail.com>
2019-06-08 17:08:09 +0500

    1

--- robot.lua
+++ robot.lua
@@ -1,7 +1,7 @@
 function dropAll()
     local storageItems = {}
-    for i=1, component.inventory_controller.getInventorySize(sides.forward) do
-        storageItems[i] = component.inventory_controller.getStackInSlot(sides.forward, i)
+    for i=1, component.inventory_controller.getInventorySize(3) do
+        storageItems[i] = component.inventory_controller.getStackInSlot(3, i)
     end
     local innerItem
     for i=1, 16 do
@@ -10,27 +10,28 @@ function dropAll()
         for key, value in pairs(storageItems) do
             if (not(innerItem == nil) and innerItem["name"] == value["name"]) then
                 robot.select(i)
-                component.inventory_controller.dropIntoSlot(sides.forward, key)
+                component.inventory_controller.dropIntoSlot(3, key)
             end
         end
     end    
 end
 
 function getAll()
-    local storageItems = {}
-    local externalCellIndex = 1
     for i=1, 16 do
         innerItem = component.inventory_controller.getStackInInternalSlot(i)
         robot.select(i)
-        for externalCellIndex=1, component.inventory_controller.getInventorySize(sides.forward) do
-            externalStoredItem = component.inventory_controller.getStackInSlot(sides.forward, externalCellIndex)
+        print(innerItem)
+        for externalCellIndex=1, component.inventory_controller.getInventorySize(3) do
+            externalStoredItem = component.inventory_controller.getStackInSlot(3, externalCellIndex)
             if not(externalStoredItem == nil) then
                 print(i)
                 if innerItem == nil then
-                    component.inventory_controller.suckFromSlot(sides.forward, externalCellIndex)
-                    local slotAvaible = innerItem["maxSize"] - innerItem["size"]
-                elseif innerItem["name"] == externalStoredItem["name"] and slotAvaible > 0 then
-                    component.inventory_controller.suckFromSlot(sides.forward, externalCellIndex, slotAvaible)
+                    component.inventory_controller.suckFromSlot(3, externalCellIndex)
+                elseif innerItem["name"] == externalStoredItem["name"] then
+                    slotAvaible = innerItem["maxSize"] - innerItem["size"]
+                    if slotAvaible > 0 then
+                        component.inventory_controller.suckFromSlot(3, externalCellIndex, slotAvaible)
+                    end
                 end
             end
         end
